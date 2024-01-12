01a0965606175cb0db8d237e0ee492630cc438fa
sarbuja <s.sarbuja1@gmail.com>
2024-01-12 10:56:37 +0000

    add watch target
    add watch OS target

---
+++ FactoMini Watch App/FactoMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactoMiniApp.swift
+//  FactoMini Watch App
+//
+//  Created by Santa Gurung on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FactoMini_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
